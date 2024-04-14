0515da05f0ab009b6c1e17a5d43a3270ee1c5f85
MJ Berends <mjr.berends@gmail.com>
2024-04-14 00:35:58 -0500

    Modify parsing regex

--- backend/codegen/lib/generate.py
+++ backend/codegen/lib/generate.py
@@ -8,24 +8,26 @@ from codegen.lib.openai import client as openai_client
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_DIR = os.path.join(THIS_DIR, '..', 'templates')
 
-CODE_PATTERN = re.compile(r"(?P<codeblock>(?:<file>(?P<filename>.+)</file>\n)?(?:```(?P<language>python)\n(?P<code>(?s:.*?))\n)```)")
+CODE_PATTERN = re.compile("""(?:
+   (?:\[\[file\]\](?P<filename>.+)\[\[\/file\]\]\s*)
+   (?:\[\[code\]\]
+     \s*\[\[lang\]\](?P<lang>.+?)\[\[\/lang\]\]
+     \s*(?P<code>.+)
+   \[\[\/code\]\])
+)""", flags=re.VERBOSE|re.DOTALL)
 
 
 def generate_code(generator, problem, language, files=None, code=None):
     with open(os.path.join(TEMPLATE_DIR, 'code.tmpl')) as f:
         template = f.read()
-        print(template)
 
     system_prompt = template.format(
         language=language,
         files=files or 'No files provided',
         code=code or 'No code provided'
     )
-    #print(system_prompt)
 
     user_prompt = f"Please solve the following problem by generating code using {language}:\n{problem}"
-
-    #print(f"User prompt: {user_prompt}")
 
     message = generator.generate(
         system_prompt=system_prompt,
@@ -40,6 +42,8 @@ def generate_openai_code(problem, language, files=None, code=None):
     files = (files or '')[:500]
     code = (code or '')[:5000] # TODO: use tiktoken to split code into tokens and get token count
     response = generate_code(generator, problem, language, files, code)
+    print(f"Response: {response}")
+    print("\nParsing code response...\n")
     return parse_code_response(response)
 
 
@@ -66,3 +70,10 @@ def test():
     """)
     code = generate_openai_code(problem, 'python')
     return code
+
+
+def test_parse():
+    with open(os.path.join(THIS_DIR, 'test_file.txt')) as f:
+        text = f.read()
+        import pdb; pdb.set_trace()
+        return parse_code_response(text)
